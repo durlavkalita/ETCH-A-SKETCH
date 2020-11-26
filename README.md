a19a73ff8ea9802e5b8966390d39e60e3b35aa08
Durlav <durlavk98@gmail.com>
2020-11-26 12:09:09 +0530

    Etch-a-Sketch
    Fully functional now. Will add a few things (maybe) later.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Etch-a-Sketch
+
+### Preview - https://durlavk.github.io/ETCH-A-SKETCH/
+
+### Used-
+- html
+- css
+- js
+
+### Todo-
+- Add more color options
--- script.js
+++ script.js
@@ -19,12 +19,39 @@ function clearGrid() {
   while (gridContainer.firstElementChild) {
     gridContainer.removeChild(gridContainer.lastElementChild);
   }
+  gridContainer.style.background = "white";
+  gridContainer.style.opacity = 1;
+  gridContainer.addEventListener("mouseover", (event)=> {
+    event.target.style.background = "white";
+    event.target.style.opacity = 1;
+  });
 };
 
 function gridSize() {
   clearGrid();
-  size = parseInt(s.value); 
+  size = parseInt(s.value) || 16; 
   createGrid(size);
+  changeColor();
 };
+
+gridSize();
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeColor() {
+  gridContainer.addEventListener("mouseover", (event)=> {
+    event.target.style.background = getRandomColor();
+    event.target.style.opacity = 
+    (Math.floor(Math.random() * 6)+5)/10;
+  });
+}
+
 
 
